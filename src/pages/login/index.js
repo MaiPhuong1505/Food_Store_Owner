abc552160a74695932e7e613a0c3d53714b90d73
MaiPhuong1505 <maiphuongpham1505@gmail.com>
2022-10-24 20:15:36 +0700

    login-register

---
+++ src/pages/login/index.js
@@ -0,0 +1,149 @@
+import React, { useState } from 'react'
+import './login.css'
+import LogoOrange from '../../assets/images/LogoOrange.png'
+import { Box, Button, TextField, } from '@mui/material'
+import InputAdornment from '@mui/material/InputAdornment';
+import EmailIcon from '@mui/icons-material/Email';
+import PasswordIcon from '@mui/icons-material/Password';
+import PersonIcon from '@mui/icons-material/Person';
+import PhoneIcon from '@mui/icons-material/Phone';
+import { authService } from '../../services/auth.services';
+
+
+const Login = () => {
+    const [isSignup, setIsSignup] = useState(false)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [phoneNumber, setPhone] = useState("")
+
+    async function handleLogin() {
+        if (isSignup) {
+            let account = {
+                name, email, password, phoneNumber
+            }
+            console.log(account)
+            try {
+                const signup = await authService.register(account)
+                if (signup){
+                    console.log(signup.data)
+                }
+            } catch (error) {
+                console.log(error.response.data)
+            }
+        } else {
+            let account = {
+                userName: email,
+                password: password
+            }
+            try {
+                const login = await authService.login(account);
+                if (login.data) {
+                    console.log(login.data)
+                }
+            } catch (error) {
+                console.log(error.response.data)
+            }
+        }
+    }
+    return (
+        <div>
+            <div id="heading">
+                <img src={LogoOrange} style={{ width: 80 }} alt="Logo" />
+                <h1>BẮT ĐẦU CÙNG FOORDER NHÉ</h1>
+            </div>
+            <div className="splitdiv">
+                <div className="leftsplit">
+                </div>
+                <div className="rightsplit">
+                    <form>
+                        <Box
+                            maxWidth={500}
+                            minWidth={400}
+                            display="flex"
+                            alignItems="center"
+                            justifyContent={"center"}
+                            flexDirection={"column"}>
+                            {isSignup && (
+                                <TextField
+                                    fullWidth
+                                    size='small' margin="normal" type={'text'}
+                                    variant='outlined' placeholder='Tên'
+                                    onChange={(e) => setName(e.target.value)}
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <PersonIcon sx={{ color: "#89D5C9" }} />
+                                            </InputAdornment>
+                                        ),
+                                    }}></TextField>)}
+                            <TextField
+                                fullWidth
+                                size='small' margin="normal" type={'email'}
+                                variant='outlined' placeholder='Email'
+                                onChange={(e) => setEmail(e.target.value)}
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <EmailIcon sx={{ color: "#89D5C9" }} />
+                                        </InputAdornment>
+                                    ),
+                                }}></TextField>
+                            <TextField
+                                fullWidth
+                                size='small' margin="normal" type={'password'}
+                                variant='outlined' placeholder='Mật khẩu'
+                                onChange={(e) => setPassword(e.target.value)}
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <PasswordIcon sx={{ color: "#89D5C9" }} />
+                                        </InputAdornment>
+                                    ),
+                                }}></TextField>
+                            {isSignup && (<TextField
+                                fullWidth
+                                size='small' margin="normal" type={'text'}
+                                variant='outlined' placeholder='Số điện thoại'
+                                onChange={(e) => setPhone(e.target.value)}
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <PhoneIcon sx={{ color: "#89D5C9" }} />
+                                        </InputAdornment>
+                                    ),
+                                }}></TextField>)}
+                            <Button
+                                onClick={handleLogin}
+                                sx={{
+                                    margin: 1,
+                                    borderRadius: 5,
+                                    backgroundColor: "#89D5C9",
+                                    fontSize: 16,
+                                    fontStyle: "bold"
+                                }}
+                                variant="contained" >
+                                {isSignup ? "ĐĂNG KÝ" : "ĐĂNG NHẬP"}
+                            </Button>
+                            <div>
+                                {isSignup ? "Đã có tài khoản?" : "Chưa có tài khoản?"}
+
+                                <Button
+                                    onClick={() => setIsSignup(!isSignup)}
+                                    sx={{
+                                        fontStyle: "semi-bold",
+                                        color: "#89D5C9"
+                                    }}
+                                >
+                                    {isSignup ? "Đăng nhập" : "Đăng ký"}
+                                </Button>
+                            </div>
+                        </Box>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/services/auth.services.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export const authService = {
+    login: async (data) => {
+        return await axios.post('https://takefoodauthentication.azurewebsites.net/SignIn',data) 
+    },
+    register : async (data) => {
+        return await axios.post('https://takefoodauthentication.azurewebsites.net/SignUp',data)
+     }
+}
+
+ 
