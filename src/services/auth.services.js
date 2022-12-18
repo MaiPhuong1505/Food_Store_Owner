658da31c4ac77260da1e42200eada0073ab2625c
MaiPhuong1505 <maiphuongpham1505@gmail.com>
2022-12-18 10:40:51 +0700

    change api sign in url

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export const authService = {
     login: async (data) => {
-        return await axios.post('https://takefoodauthentication.azurewebsites.net/SignIn', data)
+        return await axios.post('https://takefood-authentication.azurewebsites.net/SignIn', data)
     },
     register: async (data) => {
         return await axios.post('https://takefood-authentication.azurewebsites.net/SignUp', data)
